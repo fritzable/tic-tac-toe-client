1a4083c545490bc7a490eff9f8437035fd68a05e
fritzable <fritzzillad@aol.com>
2019-02-14 15:27:18 -0500

    Add current player message
    Add: Current player rotates between X and O
    Add: message about which turn it is displays

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,7 +1,15 @@
-
 // Store spaces as an array of empty strings
 const gameBoard = ['', '', '', '', '', '', '', '', '']
+
+// Store turn as a variable
 let playerTurnIs = 'X'
+
+// Function
+const currentPlayer = () => {
+  $('#user-message').text(`Player ${playerTurnIs}, it is your turn.`)
+  $('.square').trigger('reset')
+}
+
 // Function that takes an array index and replaces empty string with string of X
 const placeX = squareIndex => {
   // If space is empty
@@ -9,6 +17,7 @@ const placeX = squareIndex => {
   // Write X in space
     gameBoard[squareIndex] = 'X'
     playerTurnIs = 'O'
+    currentPlayer()
   }
 }
 
@@ -19,6 +28,7 @@ const placeO = squareIndex => {
   // Write O in space
     gameBoard[squareIndex] = 'O'
     playerTurnIs = 'X'
+    currentPlayer()
   }
 }
 
@@ -42,5 +52,7 @@ const userClicked = event => {
 }
 
 module.exports = {
-  userClicked
+  userClicked,
+  playerTurnIs,
+  currentPlayer
 }
