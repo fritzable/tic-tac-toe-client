5646995b58e9fca8f923a6368e4da464884d8e94
fritzable <fritzzillad@aol.com>
2019-02-14 11:06:51 -0500

    Add turn switching
    Add variable to track player turns

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,18 +1,26 @@
 
 // Store spaces as an array of empty strings
 const gameBoard = ['', '', '', '', '', '', '', '', '']
-
+let playerTurnIs = 'X'
 // Function that takes an array index and replaces empty string with string of X
 const placeX = squareIndex => {
   // If space is empty
   if (gameBoard[squareIndex] === '') {
   // Write X in space
     gameBoard[squareIndex] = 'X'
+    playerTurnIs = 'O'
   }
 }
 
 // Function that takes an array index and replaces empty string with string of O
-// const placeO = squareIndex =>
+const placeO = squareIndex => {
+  // If space is empty
+  if (gameBoard[squareIndex] === '') {
+  // Write O in space
+    gameBoard[squareIndex] = 'O'
+    playerTurnIs = 'X'
+  }
+}
 
 // Function that examines the board for a win condition and prints which player won
 // const isGameWon = gameBoard =>
@@ -24,8 +32,13 @@ const placeX = squareIndex => {
 const userClicked = event => {
   console.log(event.target)
   const index = event.target.id
-  placeX(index)
+  if (playerTurnIs === 'X') {
+    placeX(index)
+  } else if (playerTurnIs === 'O') {
+    placeO(index)
+  }
   console.log(gameBoard)
+  console.log(playerTurnIs)
 }
 
 module.exports = {
