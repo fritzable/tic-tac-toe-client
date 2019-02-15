a615260f6e3bc339013f85f8d939a2637733fdb9
fritzable <fritzzillad@aol.com>
2019-02-15 15:17:31 -0500

    Sign up api
    Add fields and click events to send sign up request to api.
    Add curl script file for sign up request.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,12 +3,24 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const events = require('./events.js')
+const config = require('./config.js')
+const getFormFields = require('../../lib/get-form-fields.js')
+
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
   $('.square').on('click', events.userClicked)
   events.currentPlayer()
+  $('#sign-up-form').on('submit', event => {
+    event.preventDefault()
+    const data = getFormFields(event.target)
+    $.ajax({
+      url: config.apiUrl + '/sign-up',
+      method: 'POST',
+      data: data
+    })
+  })
 })
 
 module.exports = {
