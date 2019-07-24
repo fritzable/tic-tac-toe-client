db8a5a848eca41fad29741aead9e4508d36c1816
fritzable <fritzzillad@aol.com>
2019-07-24 14:58:14 -0400

    Remove navbar set local api env to localhost remove image from scss

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 let apiUrl
 const apiUrls = {
   production: 'https://game-project-api.herokuapp.com',
-  development: 'https://game-project-api.herokuapp.com'
+  development: 'http://localhost:4741'
 }
 
 if (window.location.hostname === 'localhost') {
